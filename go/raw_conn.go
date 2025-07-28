bf2631bf97b46a03b30726366745c46f16e75173
ruoyu.chen <ruoyu.chen@ddhd.cn>
2025-07-28 10:56:03 +0800

    feat: use 4-byte header for tcp compat

---
+++ go/raw_conn.go
@@ -0,0 +1,18 @@
+package snet
+
+import "net"
+
+// 一个轻量包装，让被抢读的 4 字节还能读回来
+type rawConn struct {
+	net.Conn
+	prefix []byte
+}
+
+func (r *rawConn) Read(b []byte) (int, error) {
+	if len(r.prefix) > 0 {
+		n := copy(b, r.prefix)
+		r.prefix = r.prefix[n:]
+		return n, nil
+	}
+	return r.Conn.Read(b)
+}
